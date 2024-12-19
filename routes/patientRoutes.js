a89882cf6d88ebc7d88ae71cbdb66c24ca654cc8
Rocío <rocitoro19@outlook.com>
2024-12-19 19:34:48 -0300

    Añadido la carpeta routes y sus archivos

--- routes/patientRoutes.js
+++ routes/patientRoutes.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // routes/patientRoutes.js
 const express = require('express');
 const router = express.Router();
@@ -11,17 +10,3 @@ router.put('/:id', updatePatient); // Actualizar un paciente por ID
 router.delete('/:id', deletePatient); // Eliminar un paciente por ID
 
 module.exports = router;
-=======
-// routes/patientRoutes.js
-const express = require('express');
-const router = express.Router();
-const { createPatient, updatePatient, deletePatient, getPatients } = require('../controllers/patientController');
-
-// Rutas para pacientes
-router.get('/', getPatients); // Obtener todos los pacientes
-router.post('/', createPatient); // Crear un nuevo paciente
-router.put('/:id', updatePatient); // Actualizar un paciente por ID
-router.delete('/:id', deletePatient); // Eliminar un paciente por ID
-
-module.exports = router;
->>>>>>> 88cbee1 (Añadido la carpeta routes y sus archivos)
