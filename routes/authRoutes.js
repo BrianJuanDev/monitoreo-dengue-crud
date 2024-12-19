c1e8553cf68d674b790cc37361d3aaac474d75cc
Rocío <rocitoro19@outlook.com>
2024-12-19 20:08:08 -0300

    Resolución de conflicto en authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // routes/authRoutes.js
 const express = require('express');
 const router = express.Router();
@@ -9,15 +8,3 @@ router.post('/register', register); // Registro de usuario
 router.post('/login', login); // Login de usuario
 
 module.exports = router;
-=======
-// routes/authRoutes.js
-const express = require('express');
-const router = express.Router();
-const { register, login } = require('../controllers/authController');
-
-// Rutas de autenticación
-router.post('/register', register); // Registro de usuario
-router.post('/login', login); // Login de usuario
-
-module.exports = router;
->>>>>>> 88cbee1 (Añadido la carpeta routes y sus archivos)
