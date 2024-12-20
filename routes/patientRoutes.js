e8d28ffcc7b2b08158024b755e5b3b5e9534eb9e
Brian <Brianjuandev@gmail.com>
2024-12-19 21:43:33 -0300

    Implementar el middleware de autenticación en las rutas de paciente

--- routes/patientRoutes.js
+++ routes/patientRoutes.js
@@ -1,12 +1,13 @@
 // routes/patientRoutes.js
 const express = require('express');
 const router = express.Router();
+const authenticate = require('../middlewares/authMiddleware');
 const { createPatient, updatePatient, deletePatient, getPatients } = require('../controllers/patientController');
 
 // Rutas para pacientes
-router.get('/', getPatients); // Obtener todos los pacientes
-router.post('/', createPatient); // Crear un nuevo paciente
-router.put('/:id', updatePatient); // Actualizar un paciente por ID
+router.get('/', authenticate, getPatients); // Obtener todos los pacientes
+router.post('/', authenticate, createPatient); // Crear un nuevo paciente
+router.put('/:id', authenticate, updatePatient); // Actualizar un paciente por ID
 router.delete('/:id', deletePatient); // Eliminar un paciente por ID
 
 module.exports = router;
