13dafd5671c04138f9ecccc13dd516ad42c6ef87
Rocío <rocitoro19@outlook.com>
2025-02-28 03:40:16 -0300

    Agregada la carpeta config con archivos de configuración

---
+++ config/swagger.js
@@ -0,0 +1,26 @@
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const swaggerOptions = {
+    swaggerDefinition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'API Monitoreo Dengue',
+            version: '1.0.0',
+            description: 'Documentación de la API para gestionar pacientes con dengue',
+        },
+        servers: [
+            {
+                url: 'http://localhost:3000',
+                description: 'Servidor local',
+            },
+        ],
+    },
+    apis: ['./routes/*.js'], // Rutas para la documentación
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+
+module.exports = (app) => {
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+};
